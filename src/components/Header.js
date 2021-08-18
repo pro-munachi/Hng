128e9530753e249e026ba5eec2d9ff125096744b
pro-munachi <madustanley1@gmail.com>
2021-08-18 18:56:16 +0100

    final push

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ import './header.css'
 const Header = () => {
     return (
         <header>
-            <img src="https://hng.tech/img/brand-logo.png" alt="" />
+            <a href='https://www.zuri.team' target='_blank' rel='noopener noreferrer'>
+                <img src="https://hng.tech/img/brand-logo.png" alt="" /></a>
         </header>
     )
 }
